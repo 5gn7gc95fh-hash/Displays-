7d0f52453f954ca6305ecf5c189ae30db245e43a
AlexanderX <5gn7gc95fh@privaterelay.appleid.com>
2026-06-08 21:01:44 -0500

    Add files via upload

---
+++ sw-fix-ventas2.js
@@ -0,0 +1,4 @@
+const CACHE='fix-ventas2-20260604';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{return;});
